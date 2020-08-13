5e30aaeba8fd245cdecf63ef48f9336b0bd76843
mauriciomutte <litiobr@gmail.com>
2020-08-13 10:03:15 -0300

    🔧 add storybook v6

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,9 @@
 import * as S from './styles';
 
-const Main = () => (
+const Main = ({ title = 'nextjs-playground', description = 'Typescript, ReactJS, NextJS and styled-components' }) => (
 	<S.Wrapper>
-		<S.Title>nextjs-playground</S.Title>
-		<S.Description>Typescript, ReactJS, NextJS and styled-components</S.Description>
+		<S.Title>{title}</S.Title>
+		<S.Description>{description}</S.Description>
 		<S.Illustration src="/img/home-illustration.svg" alt="Code thinking" />
 	</S.Wrapper>
 );
