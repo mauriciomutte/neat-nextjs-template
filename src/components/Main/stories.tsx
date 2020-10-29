64e60bfd8bef6944348840cfb22392ea01843104
mauriciomutte <litiobr@gmail.com>
2020-10-28 22:55:10 -0300

    Storybook migrate Knobs to Controls

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,17 +1,10 @@
-import { Meta, Story } from '@storybook/react/types-6-0';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { Story, Meta } from '@storybook/react/types-6-0';
 
 import Main from '.';
 
 export default {
 	title: 'Main',
 	component: Main,
-	decorators: [withKnobs],
 } as Meta;
 
-export const Basic: Story = () => (
-	<Main
-		title={text('Title', 'Hello Storybook')}
-		description={text('Description', 'Typescript, ReactJS, NextJS and styled-components')}
-	/>
-);
+export const Basic: Story = (args) => <Main {...args} />;
