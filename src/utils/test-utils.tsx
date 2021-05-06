478ba201077e6c4abc387749915816ca8b25f846
mauriciomutte <litiobr@gmail.com>
2021-05-05 22:19:46 -0300

    feat(test): add test-utils

---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { render, RenderOptions } from '@testing-library/react';
+
+import theme from 'styles/theme';
+
+type CustomRenderProps = Omit<RenderOptions, 'queries'>;
+
+const customRender = (ui: ReactElement, { ...renderOptions }: CustomRenderProps = {}) =>
+	render(<ThemeProvider theme={theme}>{ui}</ThemeProvider>, renderOptions);
+
+export * from '@testing-library/react';
+export { customRender as render };
