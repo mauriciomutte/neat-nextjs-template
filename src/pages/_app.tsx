e7e956d09b53a89a44b25f1ace081a5b105ce998
mauriciomutte <litiobr@gmail.com>
2021-05-05 22:15:59 -0300

    feat(styles): add styled-components theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
 import Head from 'next/head';
 
 import GlobalStyles from 'styles/global';
+import theme from 'styles/theme';
 
 function App({ Component, pageProps }: AppProps) {
 	return (
-		<>
+		<ThemeProvider theme={theme}>
 			<Head>
 				<title>nextjs-playground</title>
 				<link rel="manifest" href="/manifest.json" />
@@ -13,7 +15,7 @@ function App({ Component, pageProps }: AppProps) {
 			</Head>
 			<GlobalStyles />
 			<Component {...pageProps} />
-		</>
+		</ThemeProvider>
 	);
 }
 
