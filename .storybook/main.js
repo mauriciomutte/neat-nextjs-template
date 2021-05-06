99e04cdde47945bad00353954ebf81a0513bcebe
mauriciomutte <litiobr@gmail.com>
2021-05-05 22:37:00 -0300

    fix(storybook): babel config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,9 @@
 module.exports = {
 	stories: ['../src/components/**/stories.tsx'],
 	addons: ['@storybook/addon-essentials'],
-	babel: async (options) => ({
-		...options,
-		plugins: [...options.plugins, '@babel/plugin-transform-react-jsx'],
+	babel: async (config) => ({
+		...config,
+		plugins: [...config.plugins, require.resolve('@babel/plugin-transform-react-jsx')],
 	}),
 	webpackFinal: (config) => {
 		config.resolve.modules.push(`${process.cwd()}/src`);
