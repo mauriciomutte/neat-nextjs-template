b11cb42dbea9568996a3d1d343b0038560b7c9e3
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-08-05 17:36:48 -0300

    chore: reset repo

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	testEnvironment: 'jsdom',
-	testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-	collectCoverage: true,
-	collectCoverageFrom: ['src/**/*.spec.ts(x)?', '!src/**/stories.tsx'],
-	setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-	modulePaths: ['<rootDir>/src/'],
-};
--- src/pages/index.tsx
+++
@@ -1,5 +0,0 @@
-import Main from 'components/Main/Main';
-
-export default function Home() {
-	return <Main />;
-}
